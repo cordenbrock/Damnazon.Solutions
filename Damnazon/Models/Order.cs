53003c3809f3c6a6970d1cf671df67b4b0eb1130
cordenbrock <cordenbrock@gmail.com>
2021-01-25 16:53:06 -0800

    Fix indentation
    Co-authored-by: Callie Stump <callie@stu.mp>
    Co-authored-by: Constantine Yakubovski <faustlarsen@gmail.com>
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

--- Damnazon/Models/Order.cs
+++ Damnazon/Models/Order.cs
@@ -7,49 +7,49 @@ using System.Threading.Tasks;
 
 namespace Damnazon.Models
 {
-      public class Order
-    {
-        [BindNever]
-        public int OrderId { get; set; }
+	public class Order
+	{
+		[BindNever]
+		public int OrderId { get; set; }
 
-        [Required(ErrorMessage = "Please enter your first name")]
-        [Display(Name = "First Name")]
-        [StringLength(25)]
-        public string FirstName { get; set; }
+		[Required(ErrorMessage = "Please enter your first name")]
+		[Display(Name = "First Name")]
+		[StringLength(25)]
+		public string FirstName { get; set; }
 
-        [Required(ErrorMessage = "Please enter your last name")]
-        [Display(Name = "Last Name")]
-        [StringLength(50)]
-        public string LastName { get; set; }
+		[Required(ErrorMessage = "Please enter your last name")]
+		[Display(Name = "Last Name")]
+		[StringLength(50)]
+		public string LastName { get; set; }
 
-        [Required(ErrorMessage = "Please enter your address")]
-        [Display(Name = "Street Address")]
-        [StringLength(100)]
-        public string Address { get; set; }
+		[Required(ErrorMessage = "Please enter your address")]
+		[Display(Name = "Street Address")]
+		[StringLength(100)]
+		public string Address { get; set; }
 
-        [Required(ErrorMessage = "Please enter your city")]
-        public string City { get; set; }
+		[Required(ErrorMessage = "Please enter your city")]
+		public string City { get; set; }
 
-        [Required(ErrorMessage = "Please enter your state")]
-        [StringLength(2,MinimumLength =2)]
-        public string State { get; set; }
+		[Required(ErrorMessage = "Please enter your state")]
+		[StringLength(2,MinimumLength =2)]
+		public string State { get; set; }
 
-        [Required(ErrorMessage = "Please enter your zip")]
-        [StringLength(5, MinimumLength = 5)]
-        [Display(Name = "Zip Code")]
-        public string ZipCode { get; set; }
+		[Required(ErrorMessage = "Please enter your zip")]
+		[StringLength(5, MinimumLength = 5)]
+		[Display(Name = "Zip Code")]
+		public string ZipCode { get; set; }
 
-        [Required(ErrorMessage = "Please enter your phone")]
-        [DataType(DataType.PhoneNumber)]
-        [Display(Name = "Phone Number")]
-        public string PhoneNumber { get; set; }
+		[Required(ErrorMessage = "Please enter your phone")]
+		[DataType(DataType.PhoneNumber)]
+		[Display(Name = "Phone Number")]
+		public string PhoneNumber { get; set; }
 
-        public List<OrderProduct> OrderProduct { get; set; }
+		public List<OrderProduct> OrderProduct { get; set; }
 
-        [BindNever]
-        public decimal OrderTotal { get; set; }
+		[BindNever]
+		public decimal OrderTotal { get; set; }
 
-        [BindNever]
-        public DateTime OrderPlaced { get; set; } 
-    }
+		[BindNever]
+		public DateTime OrderPlaced { get; set; } 
+	}
 }
