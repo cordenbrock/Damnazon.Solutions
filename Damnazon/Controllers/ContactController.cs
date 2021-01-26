cb06317f2c4fa854aab23b0d38518d9c36b9c83e
cordenbrock <cordenbrock@gmail.com>
2021-01-26 10:45:14 -0800

    Add dummy 'contact us' functionality
    Co-authored-by: Callie Stump <callie@stu.mp>
    Co-authored-by: Constantine Yakubovski <faustlarsen@gmail.com>
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

---
+++ Damnazon/Controllers/ContactController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Damnazon.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}    
