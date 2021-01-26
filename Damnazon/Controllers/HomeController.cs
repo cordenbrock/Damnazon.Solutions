2f3f130c77f617cf1ddf713f70be7de2fa17e908
cordenbrock <cordenbrock@gmail.com>
2021-01-26 11:46:25 -0800

    Pass in all the sweet deals to splash page
    Co-authored-by: Callie Stump <callie@stu.mp>
    Co-authored-by: Constantine Yakubovski <faustlarsen@gmail.com>
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

--- Damnazon/Controllers/HomeController.cs
+++ Damnazon/Controllers/HomeController.cs
@@ -5,25 +5,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Damnazon.Models;
+using Damnazon.ViewModels;
 
 namespace Damnazon.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly IProductRepository _productRepository;
+
+    public HomeController(IProductRepository productRepository)
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      _productRepository = productRepository;
     }
+    public IActionResult Index()
+    {
+      return View(new HomeViewModel
+      {
+        DamnazonsChoiceProducts = _productRepository.GetDamnazonsChoiceProducts,
+        DamnazonSlimeProducts = _productRepository.GetDamnazonSlimeProducts
+      });
+    }
+
+
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
 }
---
+++ Damnazon/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Damnazon.Models;
+
+namespace Damnazon.ViewModels
+{
+  public class HomeViewModel
+  {
+    public IEnumerable<Product> DamnazonsChoiceProducts { get; set; }
+    public IEnumerable<Product> DamnazonSlimeProducts { get; set; }
+  }
+}
