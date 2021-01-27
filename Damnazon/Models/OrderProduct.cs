4770d4fa546049966a84aa7a619c709b261c536c
cordenbrock <cordenbrock@gmail.com>
2021-01-26 17:29:09 -0800

    Set up for order form?
    Co-authored-by: Callie Stump <callie@stu.mp>
    Co-authored-by: Constantine Yakubovski <faustlarsen@gmail.com>
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

--- Damnazon/Models/OrderProduct.cs
+++ Damnazon/Models/OrderProduct.cs
@@ -3,6 +3,7 @@ namespace Damnazon.Models
   public class OrderProduct
   {
     public int OrderProductId { get; set; }
+    public string OrderProductGuid { get; set; }
     public int OrderId { get; set; }
     public int ProductId { get; set; }
     public Product Product { get; set; }
