7bef1bf3621d5062b786a7a456d311397a55bb9e
cordenbrock <cordenbrock@gmail.com>
2021-01-26 17:01:56 -0800

    Add field for images
    Co-authored-by: Callie Stump <callie@stu.mp>
    Co-authored-by: Constantine Yakubovski <faustlarsen@gmail.com>
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

--- Damnazon/Models/Category.cs
+++ Damnazon/Models/Category.cs
@@ -7,7 +7,7 @@ namespace Damnazon.Models
     public int CategoryId { get; set;}
     public string CategoryName { get; set;}
     public string CategoryDescription { get; set;}
-
+    public string ThumbnailImage { get; set; }
     public List<Product> Products { get; set; }
   }
 }
