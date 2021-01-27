b498f5fe3d234503da823bcb83cd1f1ec529d797
cordenbrock <cordenbrock@gmail.com>
2021-01-27 00:03:12 -0800

    Add view model for shopping cart that'll pass the session shopping cart model and its calculated total
    Co-authored-by: Callie Stump <callie@stu.mp>
    Co-authored-by: Constantine Yakubovski <faustlarsen@gmail.com>
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

---
+++ Damnazon/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,14 @@
+using Damnazon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Damnazon.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public ShoppingCart ShoppingCart { get; set; }
+        public decimal ShoppingCartTotal { get; set; }
+    }
+}
