85bded740831223c1d5e9dd0211b54680e8d99e3
cordenbrock <cordenbrock@gmail.com>
2021-01-26 17:06:14 -0800

    Wire up functionality for dropdown category menu
    Co-authored-by: Callie Stump <callie@stu.mp>
    Co-authored-by: Constantine Yakubovski <faustlarsen@gmail.com>
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

--- Damnazon/Components/CategoryMenu.cs
+++ Damnazon/Components/CategoryMenu.cs
@@ -7,5 +7,19 @@ using System.Threading.Tasks;
 
 namespace Damnazon.Components
 {
+    public class CategoryMenu : ViewComponent
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        public CategoryMenu(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
 
+        public IViewComponentResult Invoke()
+        {
+            var categories = _categoryRepository.GetAllCategories.OrderBy(c => c.CategoryName);
+
+            return View(categories);
+        }
+    }
 }
