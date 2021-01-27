07c91688fa1e86ef6697381ca1f9502029cc996e
cordenbrock <cordenbrock@gmail.com>
2021-01-26 17:01:18 -0800

    Add images to seed data
    Co-authored-by: Callie Stump <callie@stu.mp>
    Co-authored-by: Constantine Yakubovski <faustlarsen@gmail.com>
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

--- Damnazon/Models/DamnazonContext.cs
+++ Damnazon/Models/DamnazonContext.cs
@@ -11,16 +11,37 @@ namespace Damnazon.Models
     }
     public virtual DbSet<Order> Orders { get; set; }
     public virtual DbSet<Category> Categories { get; set; }
+    public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
     public virtual DbSet<Product> Products { get; set; }
     public virtual DbSet<OrderProduct> OrderProduct { get; set; }
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
           base.OnModelCreating(modelBuilder);
 
-          modelBuilder.Entity<Category>().HasData(new Category { CategoryId = 1, CategoryName = "Assets", CategoryDescription = "This is a tab for all of Jeff Bezos finacial assets." });
-          modelBuilder.Entity<Category>().HasData(new Category { CategoryId = 2, CategoryName = "Properties", CategoryDescription = "This is a tab for all of Jeff Bezos properties." });
-          modelBuilder.Entity<Category>().HasData(new Category { CategoryId = 3, CategoryName = "Vehicles", CategoryDescription = "This is a tab for all of Jeff Bezos vehicles." });
+          modelBuilder.Entity<Category>().HasData(new Category 
+          { 
+            CategoryId = 1,
+            CategoryName = "Assets", 
+            CategoryDescription = "This is a tab for all of Jeff Bezos finacial assets.",
+            ThumbnailImage = "\\img\\asset.jpg"
+          });
+          modelBuilder.Entity<Category>().HasData(new Category
+          {
+            CategoryId = 2,
+            CategoryName = "Properties",
+            CategoryDescription = "This is a tab for all of Jeff Bezos properties.",
+            ThumbnailImage = "\\img\\seattleLakehouse.jpg"
+          });
+          modelBuilder.Entity<Category>().HasData(new Category
+          {
+            CategoryId = 3,
+            CategoryName = "Vehicles",
+            CategoryDescription = "This is a tab for all of Jeff Bezos vehicles.",
+            ThumbnailImage = "\\img\\hondaAccord.jpg"
+          });
+
 
           modelBuilder.Entity<Product>().HasData(new Product
           {
@@ -28,7 +49,7 @@ namespace Damnazon.Models
             ProductName = "Lorem Blue Origin Stocksum",
             ProductPrice = 5.25M,
             ProductDescription = "Space bugs, everywhere, hurry, buy blue origin stock today, fight cosmic insects!",
-            Image = "",
+            Image = "\\img\\blueOrigin.jpg",
             IsDamnazonsChoice = true,
             IsDamnazonSlime = true,
             CategoryId = 1
@@ -40,7 +61,7 @@ namespace Damnazon.Models
             ProductName = "Lorem Washerington Pest",
             ProductPrice = 4.10M,
             ProductDescription = "Real news based on real true facts",
-            Image = "",
+            Image = "\\img\\washingtonPost.jpg",
             IsDamnazonsChoice = true,
             IsDamnazonSlime = false,
             CategoryId = 2
@@ -49,10 +70,10 @@ namespace Damnazon.Models
           modelBuilder.Entity<Product>().HasData(new Product
           {
             ProductId = 3,
-            ProductName = "Lorem Bezos Bicyclusum",
+            ProductName = "Lorem Bezos Jetticus jettison",
             ProductPrice = 2.5M,
             ProductDescription = "WOOOOOOOOOOOO",
-            Image = "",
+            Image = "\\img\\gulfstreamPrivateJet.jpg",
             IsDamnazonsChoice = false,
             IsDamnazonSlime = true,
             CategoryId = 3
