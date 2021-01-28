507ea9e0e19061e548e60b60dc9167802195d045
cordenbrock <cordenbrock@gmail.com>
2021-01-27 17:03:13 -0800

    Add action methods for checkout and checkout complete
    Co-authored-by: Callie Stump <callie@stu.mp>
    Co-authored-by: Constantine Yakubovski <faustlarsen@gmail.com>
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

---
+++ Damnazon/Controllers/OrderController.cs
@@ -0,0 +1,60 @@
+using Damnazon.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Damnazon.Controllers
+{
+  public class OrderController : Controller
+  {
+    private readonly IOrderRepository _orderRepository;
+    private readonly ShoppingCart _shoppingCart;
+
+    public OrderController(IOrderRepository orderRepository, ShoppingCart shoppingCart)
+    {
+      _orderRepository = orderRepository;
+      _shoppingCart = shoppingCart;
+    }
+
+    public IActionResult Checkout()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public IActionResult Checkout(Order order)
+    {
+      _shoppingCart.ShoppingCartItems = _shoppingCart.GetAllShoppingCartItems();
+
+      if (_shoppingCart.ShoppingCartItems.Count == 0)
+      {
+        ModelState.AddModelError("", "Your cart is empty");
+      }
+
+      if (ModelState.IsValid)
+        {
+          _orderRepository.CreateOrder(order);
+          _shoppingCart.ClearShoppingCart();
+          return RedirectToAction("CheckoutComplete");
+        }
+
+      return View(order);
+    }
+
+    public IActionResult CheckoutComplete()
+    {
+      ViewBag.CheckoutCompleteMessage = "Thank you for your order. Enjoy your items.";
+      return View();
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
