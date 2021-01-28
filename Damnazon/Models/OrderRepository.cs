eff531e7a184d809c007d5cceb5cf12a275b6c85
cordenbrock <cordenbrock@gmail.com>
2021-01-27 17:04:26 -0800

    Add shopping cart field and save order details to db
    Co-authored-by: Callie Stump <callie@stu.mp>
    Co-authored-by: Constantine Yakubovski <faustlarsen@gmail.com>
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

--- Damnazon/Models/OrderRepository.cs
+++ Damnazon/Models/OrderRepository.cs
@@ -9,19 +9,37 @@ namespace Damnazon.Models
   public class OrderRepository : IOrderRepository
   {
     private readonly DamnazonContext _db;
+    private readonly ShoppingCart _shoppingCart;
 
-    public OrderRepository(DamnazonContext db)
+    public OrderRepository(DamnazonContext db, ShoppingCart shoppingCart)
     {
       _db = db;
+      _shoppingCart = shoppingCart;
     }
 
     public void CreateOrder(Order order)
     {
+      order.OrderPlaced = DateTime.Now;
+      order.OrderTotal = _shoppingCart.GetShoppingCartTotalPrice();
       _db.Orders.Add(order);
       _db.SaveChanges();
+    
+    var shoppingCartItems = _shoppingCart.GetAllShoppingCartItems();
 
+    foreach(var shoppingCartItem in shoppingCartItems)
+    {
+      var orderDetail = new OrderDetail
+      {
+        Quantity = shoppingCartItem.Quantity,
+        Price = shoppingCartItem.Product.ProductPrice,
+        ProductId = shoppingCartItem.Product.ProductId,
+        OrderId = order.OrderId
+      };
+
+      _db.OrderDetails.Add(orderDetail);
+    }
+
+    _db.SaveChanges();
     }
   }
 }
-
-// later note: Shopping cart integration w/ this model
