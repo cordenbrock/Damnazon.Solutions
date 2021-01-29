95df1bec7e755439256819218a4496b121718552
cordenbrock <cordenbrock@gmail.com>
2021-01-28 16:02:17 -0800

    Remove unused using statements
    Co-authored-by: Callie Stump <callie@stu.mp>
    Co-authored-by: Constantine Yakubovski <faustlarsen@gmail.com>
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

--- Damnazon/Controllers/ProductController.cs
+++ Damnazon/Controllers/ProductController.cs
@@ -1,55 +1,52 @@
 using Damnazon.Models;
 using Damnazon.ViewModels;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Damnazon.Controllers
 {
-    public class ProductController : Controller
-    {
-        private readonly IProductRepository _productRepository;
-        private readonly ICategoryRepository _categoryRepository;
+  public class ProductController : Controller
+  {
+    private readonly IProductRepository _productRepository;
+    private readonly ICategoryRepository _categoryRepository;
 
-        public ProductController (IProductRepository productRepository, ICategoryRepository categoryRepository)
-        { 
-            _productRepository = productRepository;
-            _categoryRepository = categoryRepository;
-        }
-
-        public ViewResult Index(string category)
-        {
-        IEnumerable<Product> products;
-        string currentCategory;
-
-            if (string.IsNullOrEmpty(category))
-            {
-                products = _productRepository.GetAllProducts.OrderBy(p => p.ProductId);
-                currentCategory = "All Products";
-            }
-            else
-            {
-                products = _productRepository.GetAllProducts.Where(c => c.Category.CategoryName == category);
-
-                currentCategory = _categoryRepository.GetAllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
-            }
-
-            return View(new ProductListViewModel
-            {
-                Products = products,
-                CurrentCategory = currentCategory
-            });
-        }
-
-        public IActionResult Details(int id)
-        {
-            var product = _productRepository.GetProductById(id);
-            if (product == null)
-            return NotFound();
-
-        return View(product);
-        }
+    public ProductController (IProductRepository productRepository, ICategoryRepository categoryRepository)
+    { 
+      _productRepository = productRepository;
+      _categoryRepository = categoryRepository;
     }
+
+    public ViewResult Index(string category)
+    {
+      IEnumerable<Product> products;
+      string currentCategory;
+
+      if (string.IsNullOrEmpty(category))
+      {
+          products = _productRepository.GetAllProducts.OrderBy(p => p.ProductId);
+          currentCategory = "All Products";
+      }
+      else
+      {
+        products = _productRepository.GetAllProducts.Where(c => c.Category.CategoryName == category);
+        currentCategory = _categoryRepository.GetAllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+      }
+
+      return View(new ProductListViewModel
+      {
+        Products = products,
+        CurrentCategory = currentCategory
+      });
+    }
+
+    public IActionResult Details(int id)
+    {
+      var product = _productRepository.GetProductById(id);
+      if (product == null)
+      return NotFound();
+
+    return View(product);
+    }
+  }
 }
