4aa391bb328cc8324a261b1f69702822c1e51bb1
cordenbrock <cordenbrock@gmail.com>
2021-01-31 23:44:48 -0800

    Add props to allow for total to reflect shopping cart total

--- Damnazon/Controllers/PaymentIntentController.cs
+++ Damnazon/Controllers/PaymentIntentController.cs
@@ -1,6 +1,7 @@
 using Damnazon.Models;
 using Damnazon.ViewModels;
 using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -8,6 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json;
+using System.IO;
 using System.Threading.Tasks;
 using Stripe;
 
@@ -17,15 +19,15 @@ namespace Damnazon.Controllers
   [ApiController]
   public class PaymentIntentApiController : Controller
   {
+    private readonly IOrderRepository _orderRepository;
+    private readonly ShoppingCart _shoppingCart;
+    public PaymentIntentApiController(IOrderRepository orderRepository, ShoppingCart shoppingCart)
+    {
+      _orderRepository = orderRepository;
+      _shoppingCart = shoppingCart;
+    }
     public IActionResult Create()
     {
-      // var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
-      // Event stripeEvent = EventUtility.ParseEvent(json);
-      // StripeConfiguration.ApiKey = "obfuscated_offline_key";
-
-      // var service = new PaymentIntentService();
-      // var model = service.Get("pi_1IEXdVAj6lEpfZJJDBXnNoaX");
-
       return View();
     }
 
@@ -33,25 +35,20 @@ namespace Damnazon.Controllers
     public ActionResult Create(PaymentIntentCreateRequest request)
     {
       // Alternatively, set up a webhook to listen for the payment_intent.succeeded event
-      // and attach the PaymentMethod to a new Customer
-      var customers = new CustomerService();
-      var customer = customers.Create(new CustomerCreateOptions());
+      // // and attach the PaymentMethod to a new Customer
+      // var customers = new CustomerService();
+      // var customer = customers.Create(new CustomerCreateOptions());
 
       var paymentIntents = new PaymentIntentService();
+      var amount = _shoppingCart.GetShoppingCartTotalPrice() * 100;
+      // var shoppingCartItems = _shoppingCart.GetAllShoppingCartItems();
       var paymentIntent = paymentIntents.Create(new PaymentIntentCreateOptions
       {
-        Amount = CalculateOrderAmount(request.Items),
+        Amount = amount,
         Currency = "usd",
       });
-      // _shoppingCart.ClearShoppingCart();
+      _shoppingCart.ClearShoppingCart();
       return Json(new { clientSecret = paymentIntent.ClientSecret });
-    }
-    private int CalculateOrderAmount(Item[] items)
-    {
-      // Replace this constant with a calculation of the order's amount
-      // Calculate the order total on the server to prevent
-      // people from directly manipulating the amount on the client
-      return 1400;
     }
     public class Item
     {
